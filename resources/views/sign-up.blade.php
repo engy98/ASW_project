ac5545619a4e051fd765b45af2ab78a6cd57e607
engy98 <engynoga553459@gmail.com>
2020-11-27 17:06:36 +0200

    first commit

---
+++ resources/views/sign-up.blade.php
@@ -0,0 +1,35 @@
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="span3 well" style="margin: 50px">
+    <legend>New to WebApp? Sign up!</legend>
+    <form accept-charset="UTF-8" action="" method="post">
+        <input class="span3" name="name" placeholder="Full Name" type="text">
+        <input class="span3" name="username" placeholder="Username" type="text">
+        <input class="span3" name="password" placeholder="Password" type="password">
+        <button class="btn btn-warning" type="submit">Sign up for WebApp</button>
+    </form>
+</div>
+
+</body>
+</html>
